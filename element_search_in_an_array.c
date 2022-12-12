7f3ca8f47014820d6b3d329b87674a56fb15d4eb
22A91A05H2 <119913815+22A91A05H2@users.noreply.github.com>
2022-12-12 19:44:58 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,a[100];     scanf("%d",&n);     //int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int ele,res=0;     scanf("%d",&ele);    for(i=0;i<n;i++)     {         if(ele==a[i])         {             printf("True");             res++;             break;         }     }     if(res==0)     {         printf("False");     }
    }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,a[100];
+    scanf("%d",&n);
+    //int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int ele,res=0;
+    scanf("%d",&ele);
+   for(i=0;i<n;i++)
+    {
+        if(ele==a[i])
+        {
+            printf("True");
+            res++;
+            break;
+        }
+    }
+    if(res==0)
+    {
+        printf("False");
+    }
+   
+}
