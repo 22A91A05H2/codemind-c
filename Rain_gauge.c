6c206440550aaa70bfe45d5fb0f82a35ef74d568
22A91A05H2 <119913815+22A91A05H2@users.noreply.github.com>
2022-12-28 23:14:08 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     while(n!=0)
        {
            int x;
            scanf("%d",&x);
            if(x<3)
            {
                printf("LIGHT
    ");
            }
            else if(x>=3 && x<7)
            {
                printf("MODERATE
    ");
            }
            else
            {
                printf("HEAVY
    ");
            }
            n--;
        }
    }

---
+++ Rain_gauge.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n!=0)
+    
+    {
+        int x;
+        scanf("%d",&x);
+        if(x<3)
+        {
+            printf("LIGHT
+");
+        }
+        else if(x>=3 && x<7)
+        {
+            printf("MODERATE
+");
+        }
+        else
+        {
+            printf("HEAVY
+");
+        }
+        n--;
+    }
+}
