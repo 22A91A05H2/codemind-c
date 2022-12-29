452cb5bfc9ee4894dada555dd6cbeed16c852c1b
22A91A05H2 <119913815+22A91A05H2@users.noreply.github.com>
2022-12-29 22:29:50 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,k1=0,k2=0,a,b;     scanf("%d",&n);     //printf("0");     for(i=1;i<=n+1;i++)     {         if(i%2==1)         {             a=2*k1;             printf("%d ",a);             k1++;         }         else         {             b=k2+1;             printf("%d ",b-1);             k2++;         }     } }

---
+++ mixture_of_2_series.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,k1=0,k2=0,a,b;
+    scanf("%d",&n);
+    //printf("0");
+    for(i=1;i<=n+1;i++)
+    {
+        if(i%2==1)
+        {
+            a=2*k1;
+            printf("%d ",a);
+            k1++;
+        }
+        else
+        {
+            b=k2+1;
+            printf("%d ",b-1);
+            k2++;
+        }
+    }
+}
