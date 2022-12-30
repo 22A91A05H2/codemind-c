b31049c3ef2b401e3875a4aedf235a0fd74a12d7
22A91A05H2 <119913815+22A91A05H2@users.noreply.github.com>
2022-12-30 18:50:09 +0530

    #include<stdio.h> int main() {     int t,i,x,n,p;     scanf("%d",&t);     for(i=1;i<=t;i++)     {         scanf("%d%d",&x,&n);         p=x/10;         printf("%d ",(n*p));     }
    }

---
+++ Placement_Test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t,i,x,n,p;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++)
+    {
+        scanf("%d%d",&x,&n);
+        p=x/10;
+        printf("%d
+",(n*p));
+    }
+        
+            
+}
