ccc00b9639c04877129b9669d036cb783217600e
22A91A05H2 <119913815+22A91A05H2@users.noreply.github.com>
2023-01-09 14:57:19 +0530

    #include<stdio.h> int main() {     int phy,chem,bio,math,com;     float per;     scanf("%d%d%d%d%d",&phy,&chem,&bio,&math,&com);     per=(phy+chem+bio+math+com)*100/500;     if(per>=90)     {         printf("Grade A");         }         else if(per>=80)         {             printf("Grade B");
            }
            else if(per>=70)
            {
                printf("Grade C");
                }
                else if(per>=60) 
                {
                    printf("Grade D");
                    }
                    else if(per>=40)
                    {
                        printf("Grade E");
                        }
                        else if(per<40)
                        {
                            printf("Grade F");
                            }
        
    }

--- Grades.c
+++ Grades.c
@@ -2,31 +2,33 @@
 int main()
 {
     int phy,chem,bio,math,com;
-    float per;
-    scanf("%d%d%d%d%d",&phy,&chem,&bio,&math,&com);
+    float per; 
+    scanf("%d%d%d%d%d",&phy,&chem,&bio,&math,&com); 
     per=(phy+chem+bio+math+com)*100/500;
-    if(per>=90)
+    if(per>=90) 
     {
         printf("Grade A");
-    }
-    else if(per>=80)
-    {
-        printf("Grade B");
-    }
-    else if(per>=70)
-    {
-        printf("Grade C");
-    }
-    else if(per>=60)
-    {
-        printf("Grade D");
-    }
-    else if(per>=40)
-    {
-        printf("Grade E");
-    }
-    else if(per<40)
-    {
-        printf("Grade F");
-    }
+        }
+        else if(per>=80)
+        {
+            printf("Grade B"); 
+            
+        }
+        else if(per>=70)
+        {
+            printf("Grade C");
+            }
+            else if(per>=60) 
+            {
+                printf("Grade D");
+                }
+                else if(per>=40)
+                {
+                    printf("Grade E");
+                    }
+                    else if(per<40)
+                    {
+                        printf("Grade F");
+                        }
+    
 }
