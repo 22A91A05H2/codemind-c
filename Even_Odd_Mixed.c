f04154eb92ae4ae274acf2f4756873ee25298518
22A91A05H2 <119913815+22A91A05H2@users.noreply.github.com>
2023-01-29 18:32:18 +0530

    #include<stdio.h> int main() {     int n,c=0,d=0,e=0,x;     scanf("%d",&n);     while(n!=0){         x=n%10;         e++;         if(x%2==0)         {             c++;         }         else         d++;         n=n/10;     }     if(c==e)     printf("Even");     else if(d==e)     printf("Odd");     else     printf("Mixed"); }

---
+++ Even_Odd_Mixed.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0,d=0,e=0,x;
+    scanf("%d",&n);
+    while(n!=0){
+        x=n%10;
+        e++;
+        if(x%2==0)
+        {
+            c++;
+        }
+        else
+        d++;
+        n=n/10;
+    }
+    if(c==e)
+    printf("Even");
+    else if(d==e)
+    printf("Odd");
+    else
+    printf("Mixed");
+}
