0611a76f49ca5d005fffa75bbb6c51ab219f1de6
22A91A05H2 <119913815+22A91A05H2@users.noreply.github.com>
2023-01-29 18:25:19 +0530

    #include<stdio.h> int main() {     int n,i,a[100],c;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     c=0;     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             if(i%2==0)             {                 c=1;                 break;             }         }
        }
        if(c==0)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    }

---
+++ Strictly_ODD.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],c;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    c=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            if(i%2==0)
+            {
+                c=1;
+                break;
+            }
+        }
+       
+    }
+    if(c==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
